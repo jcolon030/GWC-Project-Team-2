b43661c61e16195681067d34ee01733e3a6daa4a
Asia Tanelus <asia.tanelus@gmail.com>
2025-10-19 13:48:13 -0400

    Adding basics

---
+++ game.py
@@ -0,0 +1,35 @@
+import pygame
+from pet import Pet
+
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 480
+
+FPS = 60
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Our Pet Game")
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.pet = Pet("Bob", (SCREEN_WIDTH // 2, (SCREEN_HEIGHT // 2) + 20))
+        self.bg_color = (247, 239, 218)
+
+    def update(self):
+        running = True
+        while running:
+            dt = self.clock.tick(FPS) / 1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.pet.update(dt)
+
+            self.screen.fill(self.bg_color)
+            self.pet.draw(self.screen)
+            pygame.display.flip()
+        pygame.quit()
+
+if __name__ == "__main__":
+    Game().update()
---
+++ pet.py
@@ -0,0 +1,47 @@
+import pygame
+import math
+
+class Pet():
+    def __init__(self, name, pos):
+        self.name = name
+        self.pos = pygame.Vector2(pos)
+        self.max_hunger = 1
+        self.hunger = self.max_hunger
+        self.max_happiness = 1
+        self.happiness = self.max_happiness
+        self.decay_rate = 0.02
+
+    def update(self, dt):
+        self.hunger = self.hunger - ((self.decay_rate / 60) * dt)
+        self.happiness = self.happiness - ((self.decay_rate / 60) * dt)
+
+        # prevents attributes from going over or under 1 when feeding
+        self.hunger = max(0.0, min(1.0, self.hunger))
+        self.happiness = max(0.0, min(1.0, self.happiness))
+
+        print(self.hunger)
+        print(self.happiness, "\n")
+    
+    def draw(self, screen):
+        s = pygame.Surface((240, 240), pygame.SRCALPHA)
+        body_col = (230, 210, 170)
+            
+        # Body + ears
+        pygame.draw.ellipse(s, body_col, (10, 30, 220, 200))
+        pygame.draw.polygon(s, body_col, [(50, 50),(80,10),(110,50)])
+        pygame.draw.polygon(s, body_col, [(130, 50),(160,10),(190,50)])
+
+        # Eyes
+        eye = (30,30,40)
+        pygame.draw.circle(s, eye, (90, 120), 8)
+        pygame.draw.circle(s, eye, (160,120), 8)
+
+        cx, cy = 125, 155
+        pygame.draw.arc(s, eye, (cx-20, cy-8, 40, 24), math.radians(20),  math.radians(160), 3)
+
+        # Center the pet sprite onto the main screen
+        rect = s.get_rect(center=self.pos)
+        screen.blit(s, rect)
+
+    
+
